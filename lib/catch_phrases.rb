21d93d5c6b9bdbd4321cda54e502b61424345a28
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-10-25 20:52:31 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,18 @@
 def mario
+<<<<<<< HEAD
  phrase = "It's-a me, Mario!"
  puts phrase
+=======
+  status = 'It\'s-a me, Mario!'
 end
+mario
+def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+>>>>>>> 929ba54f9143c6a06c02fe53188d5b3292946319
+end
+toadstool
 
+<<<<<<< HEAD
 def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
  puts status
@@ -17,3 +27,7 @@ def any_phrase(phrase)
 puts phrase 
 end
 any_phrase("phrase")
+=======
+def link
+  status = 
+>>>>>>> 929ba54f9143c6a06c02fe53188d5b3292946319
